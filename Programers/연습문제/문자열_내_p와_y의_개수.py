9d05bc1568834a5f33759f4644c5be563e800dc5
Junhong0209 <junh040209@naver.com>
2021-05-04 20:48:43 +0900

    [Update] 파일 이름 변경 및 폴더 분할

--- Programers/연습문제/문자열_내_p와_y의_개수.py
+++ Programers/연습문제/문자열_내_p와_y의_개수.py
@@ -23,10 +23,8 @@ s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False
 
 def solution(s):
     s = s.lower()                # lower함수를 사용하여 문자열에 있는 대문자를 모두 소문자로 바꾸어 준다.
-    substring1 = 'p'
-    substring2 = 'y'
 
-    cnt_p = s.count(substring1)  # 위에서 substring1 변수에 'p'를 넣었는데, s라는 변수에서 substring1 변수에 있는 것을 찾아 개수를 반환한다.
-    cnt_y = s.count(substring2)  # 위에서 substring2 변수에 'y'를 넣었는데, s라는 변수에서 substring2 변수에 있는 것을 찾아 개수를 반환한다.
+    cnt_p = s.count('p')  # s라는 변수에서 'p'를 찾아 개수를 넣는다.
+    cnt_y = s.count('y')  # s라는 변수에서 'y'를 찾아 개수를 넣는다.
 
     return (cnt_p == cnt_y)      # cnt_p == cnt_y일 경우 True를 반환하고, cnt_p == cnt_y가 아닐 경우 False를 반환한다.
